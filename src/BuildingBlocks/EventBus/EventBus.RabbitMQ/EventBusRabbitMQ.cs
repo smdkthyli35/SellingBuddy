5d63c53eb13a2556b669942bd4e0c437d062298c
zeljkosamed <smdkthyli@gmail.com>
2022-05-20 23:53:59 +0300

    RabbitMQPersistentConnection added.

---
+++ src/BuildingBlocks/EventBus/EventBus.RabbitMQ/EventBusRabbitMQ.cs
@@ -0,0 +1,38 @@
+﻿using EventBus.Base;
+using EventBus.Base.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventBus.RabbitMQ
+{
+    public class EventBusRabbitMQ : BaseEventBus
+    {
+        public EventBusRabbitMQ(EventBusConfig config, IServiceProvider serviceProvider) : base(config, serviceProvider)
+        {
+        }
+
+        public override void Publish(IntegrationEvent @event)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Subscribe<T, TH>()
+        {
+            var eventName = typeof(T).Name;
+            eventName = ProcessEventName(eventName);
+
+            if (!SubsManager.HasSubscriptionForEvent(eventName))
+            {
+
+            }
+        }
+
+        public override void UnSubscribe<T, TH>()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
