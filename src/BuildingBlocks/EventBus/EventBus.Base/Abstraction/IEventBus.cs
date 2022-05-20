e37faa3ceb5c8b3d59ed858a171e52d20e68894a
zeljkosamed <smdkthyli@gmail.com>
2022-05-20 18:13:48 +0300

    Microservice Azure ServiceBus implementation.

--- src/BuildingBlocks/EventBus/EventBus.Base/Abstraction/IEventBus.cs
+++ src/BuildingBlocks/EventBus/EventBus.Base/Abstraction/IEventBus.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace EventBus.Base.Abstraction
 {
-    public interface IEventBus
+    public interface IEventBus : IDisposable
     {
         void Publish(IntegrationEvent @event);
         void Subscribe<T, TH>() where T : IntegrationEvent where TH : IIntegrationEventHandler<T>;
