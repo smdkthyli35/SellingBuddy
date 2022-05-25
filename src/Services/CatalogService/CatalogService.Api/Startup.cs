f81b2338099c386faf74749cd14da81b2a608200
zeljkosamed <smdkthyli@gmail.com>
2022-05-25 18:21:48 +0300

    DbContextRegistration and HostExtension added. Program.cs and startup updated.

--- src/Services/CatalogService/CatalogService.Api/Startup.cs
+++ src/Services/CatalogService/CatalogService.Api/Startup.cs
@@ -1,3 +1,4 @@
+using CatalogService.Api.Extensions;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -32,6 +33,8 @@ namespace CatalogService.Api
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "CatalogService.Api", Version = "v1" });
             });
+
+            services.ConfigureDbContext(Configuration);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
