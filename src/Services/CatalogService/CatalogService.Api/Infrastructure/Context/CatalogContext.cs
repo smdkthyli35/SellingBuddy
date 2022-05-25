9ea9321dd57159216805fe9fb780ea842c24b0c1
zeljkosamed <smdkthyli@gmail.com>
2022-05-25 16:56:24 +0300

    CatalogContext and CatalogContextDesignFactory added.

---
+++ src/Services/CatalogService/CatalogService.Api/Infrastructure/Context/CatalogContext.cs
@@ -0,0 +1,27 @@
+﻿using CatalogService.Api.Core.Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace CatalogService.Api.Infrastructure.Context
+{
+    public class CatalogContext : DbContext
+    {
+        public const string DEFAULT_SCHEMA = "catalog";
+
+        public CatalogContext(DbContextOptions<CatalogContext> options) : base(options)
+        {
+        }
+
+        public DbSet<CatalogItem> CatalogItems { get; set; }
+        public DbSet<CatalogBrand> CatalogBrands { get; set; }
+        public DbSet<CatalogType> CatalogTypes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        }
+    }
+}
---
+++ src/Services/CatalogService/CatalogService.Api/Infrastructure/Context/CatalogContextDesignFactory.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CatalogService.Api.Infrastructure.Context
+{
+    public class CatalogContextDesignFactory : IDesignTimeDbContextFactory<CatalogContext>
+    {
+        public CatalogContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<CatalogContext>()
+                .UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=catalog;Persist Security Info=True;");
+
+            return new CatalogContext(optionsBuilder.Options);
+        }
+    }
+}
