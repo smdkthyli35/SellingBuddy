830cc6db7abb1ccd5582721b2ff9411107ffa942
zeljkosamed <smdkthyli@gmail.com>
2022-05-25 16:07:48 +0300

    subscribe_event_on_rabbitmq_test method added.

---
+++ tests/BuildingBlocks/EventBus.UnitTest/EventBusTests.cs
@@ -0,0 +1,56 @@
+using EventBus.Base;
+using EventBus.Base.Abstraction;
+using EventBus.Factory;
+using EventBus.UnitTest.Events.EventHandlers;
+using EventBus.UnitTest.Events.Events;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RabbitMQ.Client;
+
+namespace EventBus.UnitTest
+{
+    [TestClass]
+    public class EventBusTests
+    {
+        private ServiceCollection services;
+
+        public EventBusTests()
+        {
+            services = new ServiceCollection();
+            services.AddLogging(configure => configure.AddConsole());
+        }
+
+        [TestMethod]
+        public void subscribe_event_on_rabbitmq_test()
+        {
+            services.AddSingleton<IEventBus>(sp =>
+            {
+                EventBusConfig config = new()
+                {
+                    ConnectionRetryCount = 5,
+                    SubscriberClientAppName = "EventBus.UnitTest",
+                    DefaultTopicName = "SellingBuddyTopicName",
+                    EventBusType = EventBusType.RabbitMQ,
+                    EventNameSuffix = "IntegrationEvent",
+                    //Connection = new ConnectionFactory()
+                    //{
+                    //    HostName = "localhost",
+                    //    Port = 5672,
+                    //    UserName = "guest",
+                    //    Password = "guest"
+                    //}
+                };
+
+                return EventBusFactory.Create(config, sp);
+            });
+
+            var sp = services.BuildServiceProvider();
+
+            var eventBus = sp.GetRequiredService<IEventBus>();
+
+            eventBus.Subscribe<OrderCreatedIntegrationEvent, OrderCreatedIntegrationEventHandler>();
+            eventBus.UnSubscribe<OrderCreatedIntegrationEvent, OrderCreatedIntegrationEventHandler>();
+        }
+    }
+}
