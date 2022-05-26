e46c8338e2ddb0b952f4bf9c1c6c2d19d5cf8d4c
zeljkosamed <smdkthyli@gmail.com>
2022-05-26 17:24:13 +0300

    Refactor CatalogContextSeed.

--- src/Services/CatalogService/CatalogService.Api/Infrastructure/Context/CatalogContextSeed.cs
+++ src/Services/CatalogService/CatalogService.Api/Infrastructure/Context/CatalogContextSeed.cs
@@ -53,12 +53,10 @@ namespace CatalogService.Api.Infrastructure.Context
                 await context.SaveChangesAsync();
                 GetCatalogItemPictures(setupDirPath, picturePath);
             }
-
         }
 
         private IEnumerable<CatalogBrand> GetCatalogBrandsFromFile(string contentPath)
         {
-
             IEnumerable<CatalogBrand> GetPreconfiguredCatalogBrands()
             {
                 return new List<CatalogBrand>()
@@ -101,7 +99,7 @@ namespace CatalogService.Api.Infrastructure.Context
                 };
             }
 
-            string fileName = Path.Combine(contentPath, "CatalogTypeTextFile.txt");
+            string fileName = Path.Combine(contentPath, "CatalogTypes.txt");
 
             if (!File.Exists(fileName))
             {
@@ -121,11 +119,30 @@ namespace CatalogService.Api.Infrastructure.Context
         private IEnumerable<CatalogItem> GetCatalogItemsFromFile(string contentPath, CatalogContext context)
         {
 
-            string fileName = Path.Combine(contentPath, "CatalogItemTextFile.txt");
+            IEnumerable<CatalogItem> GetPreconfiguredItems()
+            {
+                return new List<CatalogItem>()
+                {
+                    new() { CatalogTypeId = 2, CatalogBrandId = 2, AvailableStock = 100, Description = ".NET Bot Black Hoodie", Name = ".NET Bot Black Hoodie", Price = 19.5M, PictureFileName = "1.png" },
+                    new() { CatalogTypeId = 1, CatalogBrandId = 2, AvailableStock = 100, Description = ".NET Black & White Mug", Name = ".NET Black & White Mug", Price= 8.50M, PictureFileName = "2.png" },
+                    new() { CatalogTypeId = 2, CatalogBrandId = 5, AvailableStock = 100, Description = "Prism White T-Shirt", Name = "Prism White T-Shirt", Price = 12, PictureFileName = "3.png" },
+                    new() { CatalogTypeId = 2, CatalogBrandId = 2, AvailableStock = 100, Description = ".NET Foundation T-shirt", Name = ".NET Foundation T-shirt", Price = 12, PictureFileName = "4.png" },
+                    new() { CatalogTypeId = 3, CatalogBrandId = 5, AvailableStock = 100, Description = "Roslyn Red Sheet", Name = "Roslyn Red Sheet", Price = 8.5M, PictureFileName = "5.png" },
+                    new() { CatalogTypeId = 2, CatalogBrandId = 2, AvailableStock = 100, Description = ".NET Blue Hoodie", Name = ".NET Blue Hoodie", Price = 12, PictureFileName = "6.png" },
+                    new() { CatalogTypeId = 2, CatalogBrandId = 5, AvailableStock = 100, Description = "Roslyn Red T-Shirt", Name = "Roslyn Red T-Shirt", Price = 12, PictureFileName = "7.png" },
+                    new() { CatalogTypeId = 2, CatalogBrandId = 5, AvailableStock = 100, Description = "Kudu Purple Hoodie", Name = "Kudu Purple Hoodie", Price = 8.5M, PictureFileName = "8.png" },
+                    new() { CatalogTypeId = 1, CatalogBrandId = 5, AvailableStock = 100, Description = "Cup<T> White Mug", Name = "Cup<T> White Mug", Price = 12, PictureFileName = "9.png" },
+                    new() { CatalogTypeId = 3, CatalogBrandId = 2, AvailableStock = 100, Description = ".NET Foundation Sheet", Name = ".NET Foundation Sheet", Price = 12, PictureFileName = "10.png" },
+                    new() { CatalogTypeId = 3, CatalogBrandId = 2, AvailableStock = 100, Description = "Cup<T> Sheet", Name = "Cup<T> Sheet", Price = 8.5M, PictureFileName = "11.png" },
+                    new() { CatalogTypeId = 2, CatalogBrandId = 5, AvailableStock = 100, Description = "Prism White TShirt", Name = "Prism White TShirt", Price = 12, PictureFileName = "12.png" },
+                };
+            }
+
+            string fileName = Path.Combine(contentPath, "CatalogItems.txt");
 
             if (!File.Exists(fileName))
             {
-                //return GetPreconfiguredItems();
+                return GetPreconfiguredItems();
             }
 
             var catalogTypeIdLookup = context.CatalogTypes.ToDictionary(ct => ct.Type, ct => ct.Id);
@@ -136,14 +153,14 @@ namespace CatalogService.Api.Infrastructure.Context
                 .Select(i => i.Split(','))
                 .Select(i => new CatalogItem()
                 {
-                    CatalogTypeId = catalogTypeIdLookup[i[0].Trim()],
-                    CatalogBrandId = catalogBrandIdLookup[i[1].Trim()],
+                    CatalogTypeId = catalogTypeIdLookup[i[0]],
+                    CatalogBrandId = catalogBrandIdLookup[i[1]],
                     Description = i[2].Trim('"').Trim(),
                     Name = i[3].Trim('"').Trim(),
                     Price = Decimal.Parse(i[4].Trim('"').Trim(), NumberStyles.AllowDecimalPoint, CultureInfo.InvariantCulture),
                     PictureFileName = i[5].Trim('"').Trim(),
-                    AvailableStock = string.IsNullOrEmpty(i[6].Trim()) ? 0 : int.Parse(i[6].Trim()),
-                    OnReorder = Convert.ToBoolean(i[7].Trim())
+                    AvailableStock = string.IsNullOrEmpty(i[6]) ? 0 : int.Parse(i[6]),
+                    OnReorder = Convert.ToBoolean(i[7])
                 });
 
             return fileContent;
