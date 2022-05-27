09cd63c3abacdc7cd341024edd0bd8c9456774aa
zeljkosamed <smdkthyli@gmail.com>
2022-05-27 21:54:17 +0300

    OrderPaymentSuccessIntegrationEventHandler and OrderPaymentFailedIntegrationEventHandler created.

---
+++ src/Services/NotificationService/NotificationService/IntegrationEvents/EventHandlers/OrderPaymentFailedIntegrationEventHandler.cs
@@ -0,0 +1,30 @@
+﻿using EventBus.Base.Abstraction;
+using Microsoft.Extensions.Logging;
+using NotificationService.Api.IntegrationEvents.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotificationService.IntegrationEvents.EventHandlers
+{
+    class OrderPaymentFailedIntegrationEventHandler : IIntegrationEventHandler<OrderPaymentFailedIntegrationEvent>
+    {
+        private readonly ILogger<OrderPaymentFailedIntegrationEventHandler> logger;
+
+        public OrderPaymentFailedIntegrationEventHandler(ILogger<OrderPaymentFailedIntegrationEventHandler> logger)
+        {
+            this.logger = logger;
+        }
+
+        public Task Handle(OrderPaymentFailedIntegrationEvent @event)
+        {
+            // Send Fail Notification (Sms, Email, Push)
+
+            logger.LogInformation($"Order Payment failed with OrderId: {@event.OrderId}, ErrorMessage: {@event.ErrorMessage}");
+
+            return Task.CompletedTask;
+        }
+    }
+}
