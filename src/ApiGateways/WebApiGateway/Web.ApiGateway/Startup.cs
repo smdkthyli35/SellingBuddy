79934b5bd167faefd8410b4841041a7fe41311f6
zeljkosamed <smdkthyli@gmail.com>
2022-05-31 20:30:31 +0300

    CatalogService ConsulRegistration added and implemented.

--- src/ApiGateways/WebApiGateway/Web.ApiGateway/Startup.cs
+++ src/ApiGateways/WebApiGateway/Web.ApiGateway/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 using System;
 using System.Collections.Generic;
@@ -27,6 +28,8 @@ namespace Web.ApiGateway
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
+            services.AddOcelot();
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
