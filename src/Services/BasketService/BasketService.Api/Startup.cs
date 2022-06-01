4eb304bce16c4256d0717de056662195553d8a4e
zeljkosamed <smdkthyli@gmail.com>
2022-06-02 00:32:34 +0300

    Added BasketService to ApiGateway configurations.

--- src/Services/BasketService/BasketService.Api/Startup.cs
+++ src/Services/BasketService/BasketService.Api/Startup.cs
@@ -35,6 +35,7 @@ namespace BasketService.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            ConfigureServicesExt(services);
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
