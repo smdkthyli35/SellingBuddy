f81a6693270b358f912699f17499d21466b37804
zeljkosamed <smdkthyli@gmail.com>
2022-06-03 21:50:44 +0300

    OrderService Events added.

---
+++ src/Services/OrderService/OrderService.Domain/Events/BuyerAndPaymentMethodVerifiedDomainEvent.cs
@@ -0,0 +1,22 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderService.Domain.Events
+{
+    public class BuyerAndPaymentMethodVerifiedDomainEvent : INotification
+    {
+        public Buyer Buyer { get; private set; }
+        public PaymentMethod Payment { get; private set; }
+        public int OrderId { get; private set; }
+
+        public BuyerAndPaymentMethodVerifiedDomainEvent(Buyer buyer, PaymentMethod payment, int orderId)
+        {
+            Buyer = buyer;
+            Payment = payment;
+            OrderId = orderId;
+        }
+    }
