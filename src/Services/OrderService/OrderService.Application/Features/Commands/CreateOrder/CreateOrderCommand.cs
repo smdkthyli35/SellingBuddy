aba09a3c37ec76aeb3c0d4c19925252f981c7f73
zeljkosamed <smdkthyli@gmail.com>
2022-06-05 20:41:44 +0300

    CreateOrderCommand and CommandHandler added.

---
+++ src/Services/OrderService/OrderService.Application/Features/Commands/CreateOrder/CreateOrderCommand.cs
@@ -0,0 +1,71 @@
+﻿using MediatR;
+using OrderService.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderService.Application.Features.Commands.CreateOrder
+{
+    public class CreateOrderCommand : IRequest<bool>
+    {
+        private readonly List<OrderItemDTO> _orderItems;
+
+        public string UserName { get; set; }
+        public string City { get; set; }
+        public string Street { get; set; }
+        public string State { get; set; }
+        public string Country { get; set; }
+        public string ZipCode { get; set; }
+        public string CardNumber { get; set; }
+        public string CardHolderName { get; set; }
+        public DateTime CardExpiration { get; set; }
+        public string CardSecurityNumber { get; set; }
+        public int CardTypeId { get; set; }
+
+        public IEnumerable<OrderItemDTO> OrderItems => _orderItems;
+        public string CorrelationId { get; set; }
+
+        public CreateOrderCommand()
+        {
+            _orderItems = new List<OrderItemDTO>();
+        }
+
+        public CreateOrderCommand(List<BasketItem> basketItems, string userId, string userName, string city, string street, string state, string country, string cardNumber, string zipCode, string cardHolderName, DateTime cardExpiration, string cardSecurityNumber, int cardTypeId, string correlationId)
+        {
+            var dtoList = basketItems.Select(item => new OrderItemDTO()
+            {
+                ProductId = item.ProductId,
+                ProductName = item.ProductName,
+                PictureUrl = item.PictureUrl,
+                UnitPrice = item.UnitPrice,
+                Units = item.Quantity
+            });
+
+            _orderItems = dtoList.ToList();
+
+            UserName = userName;
+            City = city;
+            Street = street;
+            State = state;
+            Country = country;
+            ZipCode = zipCode;
+            CardNumber = cardNumber;
+            CardHolderName = cardHolderName;
+            CardExpiration = cardExpiration;
+            CardSecurityNumber = cardSecurityNumber;
+            CardTypeId = cardTypeId;
+            CorrelationId = correlationId;
+        }
+
+        public class OrderItemDTO
+        {
+            public int ProductId { get; set; }
+            public string ProductName { get; set; }
+            public decimal UnitPrice { get; set; }
+            public int Units { get; set; }
+            public string PictureUrl { get; set; }
+        }
+    }
+}
---
+++ src/Services/OrderService/OrderService.Application/Features/Commands/CreateOrder/CreateOrderCommandHandler.cs
@@ -0,0 +1,44 @@
+﻿using EventBus.Base.Abstraction;
+using MediatR;
+using OrderService.Application.IntegrationEvents;
+using OrderService.Application.Interfaces.Repositories;
+using OrderService.Domain.AggregateModels.OrderAggregate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace OrderService.Application.Features.Commands.CreateOrder
+{
+    public class CreateOrderCommandHandler : IRequestHandler<CreateOrderCommand, bool>
+    {
+        private readonly IOrderRepository orderRepository;
+        private readonly IEventBus eventBus;
+
+        public CreateOrderCommandHandler(IOrderRepository orderRepository, IEventBus eventBus)
+        {
+            this.orderRepository = orderRepository;
+            this.eventBus = eventBus;
+        }
+
+        public async Task<bool> Handle(CreateOrderCommand request, CancellationToken cancellationToken)
+        {
+            var addr = new Address(request.Street, request.City, request.State, request.Country, request.ZipCode);
+
+            Order dbOrder = new(request.UserName, addr, request.CardTypeId, request.CardNumber, request.CardSecurityNumber, request.CardHolderName, request.CardExpiration, null);
+
+            request.OrderItems.ToList().ForEach(i => dbOrder.AddOrderItem(i.ProductId, i.ProductName, i.UnitPrice, i.PictureUrl, i.Units));
+
+            await orderRepository.AddAsync(dbOrder);
+            await orderRepository.UnitOfWork.SaveEntitiesAsync(cancellationToken);
+
+            var orderStartedIntegrationEvent = new OrderStartedIntegrationEvent(request.UserName);
+
+            eventBus.Publish(orderStartedIntegrationEvent);
+
+            return true;
+        }
+    }
+}
