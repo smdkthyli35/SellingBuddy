f5eef107833a5ef3a924b41941f4370238165da4
zeljkosamed <smdkthyli@gmail.com>
2022-06-06 21:25:43 +0300

    BuyerRepository and OrderRepository added.

---
+++ src/Services/OrderService/OrderService.Infrastructure/Repositories/OrderRepository.cs
@@ -0,0 +1,32 @@
+﻿using OrderService.Application.Interfaces.Repositories;
+using OrderService.Domain.AggregateModels.OrderAggregate;
+using OrderService.Infrastructure.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderService.Infrastructure.Repositories
+{
+    public class OrderRepository : GenericRepository<Order>, IOrderRepository
+    {
+        private readonly OrderDbContext dbContext;
+
+        public OrderRepository(OrderDbContext dbContext) : base(dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public override async Task<Order> GetByIdAsync(Guid id, params Expression<Func<Order, object>>[] includes)
+        {
+            var entity = await base.GetByIdAsync(id, includes);
+
+            if (entity is null)
+                entity = dbContext.Orders.Local.FirstOrDefault(i => i.Id == id);
+
+            return entity;
+        }
+    }
+}
