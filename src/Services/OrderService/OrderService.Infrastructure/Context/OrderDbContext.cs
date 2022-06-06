434317cf02bc4785fb6a784298e0783910d769ad
zeljkosamed <smdkthyli@gmail.com>
2022-06-06 21:53:27 +0300

    OrderDbContext SaveEntitiesAsync added.

--- src/Services/OrderService/OrderService.Infrastructure/Context/OrderDbContext.cs
+++ src/Services/OrderService/OrderService.Infrastructure/Context/OrderDbContext.cs
@@ -1,8 +1,10 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
 using OrderService.Domain.AggregateModels.BuyerAggregate;
 using OrderService.Domain.AggregateModels.OrderAggregate;
 using OrderService.Domain.SeedWork;
 using OrderService.Infrastructure.EntityConfigurations;
+using OrderService.Infrastructure.Extensions;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,6 +17,17 @@ namespace OrderService.Infrastructure.Context
     public class OrderDbContext : DbContext, IUnitOfWork
     {
         public const string DEFAULT_SCHEMA = "ordering";
+        private readonly IMediator mediator;
+
+        public OrderDbContext() : base()
+        {
+
+        }
+
+        public OrderDbContext(DbContextOptions<OrderDbContext> options, IMediator mediator) : base(options)
+        {
+            this.mediator = mediator;
+        }
 
         public DbSet<Order> Orders { get; set; }
         public DbSet<OrderItem> OrderItems { get; set; }
@@ -23,9 +36,12 @@ namespace OrderService.Infrastructure.Context
         public DbSet<CardType> CardTypes { get; set; }
         public DbSet<OrderStatus> OrderStatus { get; set; }
 
-        public Task<bool> SaveEntitiesAsync(CancellationToken cancellationToken = default)
+        public async Task<bool> SaveEntitiesAsync(CancellationToken cancellationToken = default)
         {
-            throw new NotImplementedException();
+            await mediator.DispatchDomainEventAsync(this);
+
+            await base.SaveChangesAsync(cancellationToken);
+            return true;
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
